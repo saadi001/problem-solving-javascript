40a30a35ddf653a151f0cbc3da8207d4748e6104
Sheikh sadi <you@example.com>
2022-08-04 16:46:54 -0700

    problem2 added

---
+++ function-problem2.js
@@ -0,0 +1,9 @@
+// taking 3 arguments and doing sum of them 
+
+function sum(a,b,c){
+    const total = a + b + c;
+    return total;
+}
+
+const totalSum = sum(10,20,30);
+console.log('Your sum is : ', totalSum);
