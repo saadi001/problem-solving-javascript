1be48724af7bd5392b561cf3ed68124dffca2f2b
Sheikh sadi <you@example.com>
2022-08-04 15:55:03 -0700

    first commit

---
+++ function-problem1.js
@@ -0,0 +1,9 @@
+// getting square value by function
+
+function square(value){
+    const getSquare = value * value;
+    return getSquare;
+}
+
+const ValueGiven = square(1);
+console.log('Your square value is : ', ValueGiven);
