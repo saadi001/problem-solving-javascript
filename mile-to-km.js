bc6d04978335f00a945e664a418e27460e156592
Sheikh sadi <you@example.com>
2022-08-05 04:17:42 -0700

    mile to km added

---
+++ mile-to-km.js
@@ -0,0 +1,8 @@
+// meter to kilometer
+
+function mileToKm(mile) {
+    const kilometer = mile * 1.6;
+    return kilometer;
+}
+const totalKilometer = mileToKm(400);
+console.log(totalKilometer);
---
+++ problem6.js
@@ -0,0 +1,10 @@
+/* a fundtion that print multiplication of 13*/
+
+function multiplication(value) {
+    for(i=0; i<=10; i++){
+        var result = 13 * i ;
+        console.log(result);
+    }
+}
+
+var finalmultiplication = multiplication(13);
