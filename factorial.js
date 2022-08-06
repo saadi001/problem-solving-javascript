b8d898d212e121b5980d0c2384ac59c8410606dc
Sheikh sadi <you@example.com>
2022-08-05 19:12:00 -0700

    factorial added

---
+++ factorial.js
@@ -0,0 +1,12 @@
+// factorial 
+
+function getFactorial (value){
+    let result = 1;
+    for(i=value; i>=1; i--){
+        const index = i;
+        result = result * index;
+    }
+    console.log(result);
+}
+
+getFactorial(10);
