9f0e6ef0590ada4bedc36c9572d6874e166dd1d0
Sheikh sadi <you@example.com>
2022-08-07 18:10:28 -0700

    minimum wit arrays added

---
+++ minimunFromArray.js
@@ -0,0 +1,16 @@
+// finding minimum value from an array 
+
+function minValue (values){
+    let minimum =  values[0];
+    for(i=0; i<values.length; i++){
+            let element = values[i];
+            if(minimum > element){
+                minimum = element;
+            }
+    }
+    return minimum;
+
+}
+const array = [23, 11, 45, 37,7];
+const output = minValue(array);
+console.log(output);
